10654b186d729dadfc9eb64431005d14463eebff
Diógenes Silva Pedro <diogenes.pedro06@gmail.com>
2021-03-16 17:05:26 -0300

    Update sortings.c

--- Sortings Review/sources/sortings.c
+++ Sortings Review/sources/sortings.c
@@ -91,7 +91,7 @@ void mergeSort(int *arr, int start, int end){
 // Function that does bubbleSort 
 void bubbleSort(int *arr, int length){
     int swap;
-    for (int i = 0; i < length; i++){
+    for (int i = 0; i < length - 1; i++){
         for (int j = 0; j < length - i - 1; j++){
             if (arr[j] > arr[j+1]){
                 swap = arr[j];
